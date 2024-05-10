677102199e98bf910275f1c6e08b70e01a6e969f
keeferwu <keefer@qq.com>
2024-05-10 10:18:04 +0800

    update plugged.tar.gz to plugin-2024-05-10

--- install.sh
+++ install.sh
@@ -585,7 +585,7 @@ function install_vim_plugin()
 {
     PLUGDIR=${PWD}/plugged
 
-    wget https://gitee.com/keeferwu/vimplus/releases/download/plugin-2024-04-30/plugged.tar.gz
+    wget https://gitee.com/keeferwu/vimplus/releases/download/plugin-2024-05-10/plugged.tar.gz
     if [ $? -eq 0 ]; then
         if [ -d $PLUGDIR ]; then
             echo -e "\033[33m Remove old $PLUGDIR directory \033[0m"
