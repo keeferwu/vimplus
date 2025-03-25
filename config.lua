84ab69353d27a569fb7eab83fd9f0cb063d2de0c
keefer <keeferwu@qq.com>
2025-03-25 14:06:14 +0800

    update nvim-treesitter config

--- config.lua
+++ config.lua
@@ -2,7 +2,18 @@
 require('nvim-treesitter.configs').setup {
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = { "vimdoc", "vim", "luadoc", "lua", "c", "cpp", "make", "kconfig", "markdown" },
+  ensure_installed = {
+      "vimdoc",
+      "bash",
+      "c",
+      "cpp",
+      "make",
+      "yaml",
+      "markdown",
+      "python",
+      "rust",
+      "lua",
+  },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
   -- 启用代码高亮功能
